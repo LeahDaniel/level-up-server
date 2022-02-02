7ec7fe22b0467b75c28cee2ef2e6ff4edb05cc2b
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-02 13:08:02 -0600

    chap 14 complete

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import serializers
 from rest_framework import status
 from levelupapi.models import Event, Gamer, Game
@@ -32,8 +33,14 @@ class EventView(ViewSet):
         events = Event.objects.all()
 
         game = request.query_params.get('game', None)
+        gamer = Gamer.objects.get(user=request.auth.user)
         if game is not None:
             events = events.filter(game_id=game)
+
+        # Set the `joined` property on every event
+        for event in events:
+            # Check to see if the gamer is in the attendees list on the event
+            event.joined = gamer in event.attendees.all()
 
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
@@ -74,11 +81,29 @@ class EventView(ViewSet):
         event.save()
 
         return Response(None, status=status.HTTP_204_NO_CONTENT)
-    
+
     def destroy(self, request, pk):
         event = Event.objects.get(pk=pk)
         event.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.add(gamer)
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """delete request for a user to leave an event"""
+
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.remove(gamer)
+        return Response({'message': 'Gamer removed'}, status=status.HTTP_204_NO_CONTENT)
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -87,4 +112,6 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         depth = 2
-        fields = "__all__"
+        fields = ('id', 'game', 'organizer',
+                  'description', 'date', 'time', 'attendees',
+                  'joined')
