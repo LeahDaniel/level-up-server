f342980c552d348ea5f3bb5b8c5524cf3c855c2b
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-23 10:49:20 -0600

    chap 4 complete- django sql and templates

---
+++ levelupreports/views/users/eventsbyuser.py
@@ -0,0 +1,95 @@
+"""Module for generating games by user report"""
+from django.shortcuts import render
+from django.db import connection
+from django.views import View
+
+from levelupreports.views.helpers import dict_fetch_all
+
+
+class UserEventList(View):
+    def get(self, request):
+        with connection.cursor() as db_cursor:
+
+            # TODO: Write a query to get all events along with the attendee first name, last name, and id
+            db_cursor.execute("""
+            SELECT  
+            u.first_name || " " || u.last_name as full_name,
+            g.id gamer_id,
+            e.id,
+            e.date,
+            e.time,
+            game.title game_name
+            FROM levelupapi_eventgamer eg
+            JOIN levelupapi_gamer g ON g.id = eg.gamer_id
+            JOIN levelupapi_event e ON e.id = eg.event_id
+            JOIN levelupapi_game game ON e.game_id = game.id
+            JOIN auth_user u ON g.user_id = u.id
+            """)
+            # Pass the db_cursor to the dict_fetch_all function to turn the fetch_all() response into a dictionary
+            dataset = dict_fetch_all(db_cursor)
+
+            # Take the flat data from the dataset, and build the
+            # following data structure for each gamer.
+            # This will be the structure of the events_by_user list:
+            #
+            # [
+            #   {
+            #     "gamer_id": 1,
+            #     "full_name": "Molly Ringwald",
+            #     "events": [
+            #       {
+            #         "id": 5,
+            #         "date": "2020-12-23",
+            #         "time": "19:00",
+            #         "game_name": "Fortress America"
+            #       }
+            #     ]
+            #   }
+            # ]
+
+            events_by_user = []
+
+            for row in dataset:
+                event = {
+                    "id": row["id"],
+                    "date": row["date"],
+                    "time": row["time"],
+                    "game_name": row["game_name"],
+                }
+                
+                # This is using a generator comprehension to find the user_dict in the events_by_user list
+                # The next function grabs the dictionary at the beginning of the generator, if the generator is empty it returns None
+                # This code is equivalent to:
+                # user_dict = None
+                # for user_event in events_by_user:
+                #     if user_event['gamer_id'] == row['gamer_id']:
+                #         user_dict = user_event
+                
+                user_dict = next(
+                    (
+                        user_event for user_event in events_by_user
+                        if user_event['gamer_id'] == row['gamer_id']
+                    ),
+                    None
+                )
+                
+                if user_dict:
+                    # If the user_dict is already in the events_by_user list, append the game to the games list
+                    user_dict['events'].append(event)
+                else:
+                    # If the user is not on the events_by_user list, create and add the user to the list
+                    events_by_user.append({
+                        "gamer_id": row['gamer_id'],
+                        "full_name": row['full_name'],
+                        "events": [event]
+                    })
+        
+        # The template string must match the file name of the html template
+        template = 'users/list_with_events.html'
+        
+        # The context will be a dictionary that the template can access to show data
+        context = {
+            "userevent_list": events_by_user
+        }
+
+        return render(request, template, context)
